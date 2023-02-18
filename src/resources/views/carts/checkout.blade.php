7a5e7d61a29a236f7af9cc6d162661357c8cccbd
白石拓 <s.taku0718@gmail.com>
2023-02-18 23:26:07 +0900

    layouts create

--- src/resources/views/carts/checkout.blade.php
+++ src/resources/views/carts/checkout.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.app')
 @section('content')
-購入ありがとうございました！
-
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 my-3 text-center">
+            <div class="row">
+                <div class="col-md-10 mx-auto">
+                    <h1 class="headline">ご購入ありがとうございました。</h1>
+                    <br>
+                    <h4>登録して頂いたメールアドレスに</h4>
+                    <h4>購入商品のメールが来ます。</h4>
+                    <div class="row mt-3">
+                        <div class="col-md-10 mx-auto">
+                        </div>
+                    </div>
+                    <img src="/image/ramen.jpg" width="600" height="400">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
